6945669a72136fcce4ef9d511815916b2526db5f
jefti <jeftimeira@gmail.com>
2023-05-06 12:26:21 -0300

    atualizando nome usuario

--- src/componentes/User.js
+++ src/componentes/User.js
@@ -1,11 +1,21 @@
+import React from "react";
+import useState from "react";
+
 export default function User(){
+const [nome, setNome] = React.useState('catanacomics');
+    function registraUsuario(){
+        const nome = prompt('informe um nome de usuario :');
+        if (nome){
+            setNome(nome);
+        }
+    }
     return (
         <div class="usuario">
             <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
             <div class="texto">
             <span>
-                <strong>catanacomics</strong>
-                <ion-icon name="pencil"></ion-icon>
+                <strong>{nome}</strong>
+                <ion-icon name="pencil" onClick={registraUsuario}></ion-icon>
             </span>
             </div>
         </div>
