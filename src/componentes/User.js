2d0c394d758d8968e12cd40fefb98363ca302fc4
jefti <jeftimeira@gmail.com>
2023-05-06 12:29:50 -0300

    Imagem de usuario atualizavel

--- src/componentes/User.js
+++ src/componentes/User.js
@@ -3,15 +3,24 @@ import useState from "react";
 
 export default function User(){
 const [nome, setNome] = React.useState('catanacomics');
+const [imagem, setImagem] = React.useState('assets/img/catanacomics.svg');
+
     function registraUsuario(){
         const nome = prompt('informe um nome de usuario :');
         if (nome){
             setNome(nome);
         }
     }
+
+    function trocaImagem(){
+        const link = prompt('informe um link válido :');
+        if (link){
+            setImagem(link);
+        }
+    }
     return (
         <div class="usuario">
-            <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
+            <img src={imagem} alt="imagem de perfil" onClick={trocaImagem}/>
             <div class="texto">
             <span>
                 <strong>{nome}</strong>
