44144bbe27f3725b94b5cac95812aeb4e0bc5cbe
jefti <jeftimeira@gmail.com>
2023-05-06 14:04:41 -0300

    like implementado

--- src/componentes/Post.js
+++ src/componentes/Post.js
@@ -1,7 +1,34 @@
+import React from "react";
+import { useState } from "react";
+
 export default function Post(props){
+  const {userName, userImg, postImg, Like, likeNum, likeImg} = props.info;
+
+   const [heart, setHeart] = React.useState('heart-outline');
+   const [bookmark, setBookMark] = React.useState('bookmark-outline');
+  const [NumeroLikes,setLikes] = React.useState(likeNum);
+
     
-    const {userName, userImg, postImg, Like, likeNum, likeImg} = props.info;
+    function mudarHeart(){
+      if(heart === 'heart-outline' ){
+        setHeart('heart');
+        setLikes(Number(NumeroLikes)+1);
+
+      } else {
+        setHeart('heart-outline')
+        setLikes(Number(NumeroLikes)-1);
+      }
+    }
     
+    function mudarBookMark(){
+      if(bookmark === 'bookmark-outline' ){
+        setBookMark('bookmark')
+      } else {
+        setBookMark('bookmark-outline')
+      }
+    }
+
+
     return (
         <div class="post">
         <div class="topo">
@@ -15,25 +42,25 @@ export default function Post(props){
         </div>
 
         <div class="conteudo">
-          <img src={postImg} alt={userName}/>
+          <img src={postImg} alt={userName} onClick={mudarHeart}/>
         </div>
 
         <div class="fundo">
           <div class="acoes">
             <div>
-              <ion-icon name="heart-outline"></ion-icon>
+              <ion-icon name={heart} onClick={mudarHeart} ></ion-icon>
               <ion-icon name="chatbubble-outline"></ion-icon>
               <ion-icon name="paper-plane-outline"></ion-icon>
             </div>
             <div>
-              <ion-icon name="bookmark-outline"></ion-icon>
+              <ion-icon name={bookmark} onClick={mudarBookMark}></ion-icon>
             </div>
           </div>
 
           <div class="curtidas">
             <img src={likeImg} alt="respondeai"/>
             <div class="texto">
-                Curtido por <strong>{Like}</strong> e <strong>outras {likeNum} pessoas</strong>
+                Curtido por <strong>{Like}</strong> e <strong>outras <>{NumeroLikes}</> pessoas</strong>
             </div>
           </div>
         </div>
